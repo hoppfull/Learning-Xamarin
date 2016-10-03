b8123fa492496a190d483b07067bd9758775970d
bjorn kihlberg <hoppfull@gmail.com>
2016-10-03 16:50:41 +0200

    implemented basic client server example for c# console apps

---
+++ ex - ClientServer/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Client {
+    class Program {
+        static void Main(string[] args) {
+            HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:8000/") };
+            
+            HttpResponseMessage res = client.GetAsync("/myget").Result;
+            
+            // This is case for when response is a string: (See more general example with byte[])
+            string responseMsg = res.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(responseMsg);
+        }
+    }
+}
---
+++ ex - ClientServer/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace Server {
+    class Program {
+        static void Main(string[] args) {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8000/");
+            listener.Start();
+
+            while (true) {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest req = context.Request;
+                HttpListenerResponse res = context.Response;
+                if (req.HttpMethod == "GET" && req.RawUrl == "/myget") {
+                    Console.WriteLine("Received request!");
+
+                    //res.ContentType = "application/text";
+                    //res.StatusCode = 200;
+
+                    string responseMsg = "Message from server!";
+
+                    res.OutputStream.Write(Encoding.UTF8.GetBytes(responseMsg), 0, Encoding.UTF8.GetByteCount(responseMsg));
+                    res.Close();
+                }
+            }
+        }
+    }
+}
