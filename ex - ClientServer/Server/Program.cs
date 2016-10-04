d65766ce5730315477d9d377a9313a2cff5b3cbf
bjorn kihlberg <hoppfull@gmail.com>
2016-10-04 10:49:03 +0200

    deleted defective android project

--- ex - ClientServer/Server/Program.cs
+++ ex - ClientServer/Server/Program.cs
@@ -5,14 +5,16 @@ using System.Text;
 namespace Server {
     class Program {
         static void Main(string[] args) {
+            // This doesn't work outside the local workstation (e.g. on a LAN):
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://localhost:8000/");
             listener.Start();
-
+            
             while (true) {
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest req = context.Request;
                 HttpListenerResponse res = context.Response;
+                Console.WriteLine("tjenare");
                 if (req.HttpMethod == "GET" && req.RawUrl == "/myget") {
                     Console.WriteLine("Received request!");
 
